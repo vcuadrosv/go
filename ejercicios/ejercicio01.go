af63d845332250aeae266f76232e2e0cdcbe9be9
vcuadrosv <valery.05@hotmail.com>
2023-08-01 20:27:11 -0500

    Siguiente Commit

---
+++ ejercicios/ejercicio01.go
@@ -0,0 +1,18 @@
+package ejercicios
+
+import (
+	"strconv"
+)
+
+func DevolverDosValores(parametro string) (int, string) {
+	numero, err := strconv.Atoi(parametro)
+	if err != nil {
+		// Si hubo un error en la conversión, retornamos el valor cero y el mensaje de error
+		return 0, "Error en la conversión"
+	}
+	if numero > 100 {
+		return numero, "Es mayor a 100"
+	} else {
+		return numero, "Es menor a 100"
+	}
+}
--- main.go
+++ main.go
@@ -2,14 +2,35 @@ package main
 
 import (
 	"fmt"
+	"runtime"
 
-	"github.com/vcuadrosv/go/variables"
+	/*"github.com/vcuadrosv/go/variables"*/
+	"github.com/vcuadrosv/go/ejercicios"
 )
 
 func main() {
-	variables.MuestroEnteros()
+	/*variables.MuestroEnteros()
 	variables.RestoVariables()
 	estado, texto := variables.ConvirtiendoaTexto(1588)
 	fmt.Println(estado)
-	fmt.Println(texto)
+	fmt.Println(texto)*/
+	if os := runtime.GOOS; os == "linux" || os == "darwin" {
+		fmt.Println("Sistema Operativo: ", os)
+	} else {
+		fmt.Println("Esto es Windows")
+	}
+	/*Para realizarlo con multiples variables*/
+	/*switch os := runtime.GOOS; os {
+	case "linux":
+		fmt.Println("Esto es:", os)
+	case "darwin":
+		fmt.Println("Esto es:", os)
+	default:
+		fmt.Printf("%s \n", os)*/
+
+	/*Ejercicio*/
+
+	numero, mensaje := ejercicios.DevolverDosValores("mAÑANA")
+	fmt.Println("Número:", numero)
+	fmt.Println("Mensaje:", mensaje)
 }
